b61b1d01aba1c2f9ffba636748586c93f923351b
m.rau <m.rau@house-of-communication.com>
2023-06-19 21:54:10 +0200

    wip

--- opstate.py
+++ opstate.py
@@ -104,7 +104,8 @@ def main():
         print(f"\rapply changes ... continue now   ")
         run(["git", "pull", "--rebase=true"])
         print("restarting")
-        os.execl('sudo', sys.executable, __file__, 'apply')
+        os.execl('/usr/bin/sudo', sys.executable, sys.executable, __file__,
+                 'apply')
     elif retcode == 0:
         print("Nothing to do!")
     print("Exiting.\n")
